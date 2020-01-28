59f74ddce44441a58959aade144d8f7108ee8b6e
Ryan Meinzer <ryanmeinzer@ryanmeinzer.com>
2020-01-27 20:17:45 -0800

    18 passed

--- index.js
+++ index.js
@@ -208,8 +208,9 @@ for(let i = 0; i < inventory.length; i++){
  *     (1) an array which is an inventory of cars like the one inside /data/inventory.js.
  * sortCarInventory returns an inventory that is sorted by car_model, ascending [A-Z].
 */
-function sortCarInventory(/* code here */) {
-  /* code here */
+function sortCarInventory(inventory) {
+  return inventory.sort((a, b) => 
+  (a.car_model > b.car_model ? 1 : -1))
 }
 
 /**
@@ -221,8 +222,12 @@ function sortCarInventory(/* code here */) {
  *     (1) an array which is an inventory of cars like the one inside /data/inventory.js.
  * getModelYears returns an array containing all the 'car_year's in the inventory.
 */
-function getModelYears(/* code here */) {
-  /* code here */
+function getModelYears(inventory){
+  let yearArray = [];
+  for(let i = 0; i < inventory.length; i++){
+    yearArray.push(inventory[i].car_year);
+  }
+  return yearArray;
 }
 
 /**
@@ -237,8 +242,14 @@ function getModelYears(/* code here */) {
  * with a `car_year` which is at most the given desired max year,
  * in the same order as they appear in the original inventory.
 */
-function getOlderCars(/* code here */) {
-  /* code here */
+function getOlderCars(inventory, year) {
+let carByYears = [];
+  for(let i = 0; i < inventory.length; i++){
+    if(inventory[i].car_year <= year){
+      carByYears.push(inventory[i]);
+    }
+  }
+        return carByYears; 
 }
 
 /**
@@ -252,8 +263,14 @@ function getOlderCars(/* code here */) {
  * made by either `Audi` or `Mercedes-Benz` or `Volkswagen` or `BMW`,
  * in the same order as they appear in the original inventory.
 */
-function getGermanCars(/* code here */) {
-  /* code here */
+function getGermanCars(inventory) {
+  let gCars = [];
+  for(let i = 0; i < inventory.length; i++){
+    if(inventory[i].car_make === 'Audi' || inventory[i].car_make === 'Mercedes-Benz' || inventory[i].car_make === 'Volkswagen' || inventory[i].car_make === 'BMW') {
+      gCars.push(inventory[i]);
+    }
+  }
+  return gCars;
 }
 
 /**
